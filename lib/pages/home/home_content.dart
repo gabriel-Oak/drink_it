f325eb1d6f3ecedc06a3cb1b33b19c63a10f00c6
gabriel-Oak <gabrielcarvalhocosta@live.com>
2022-11-29 00:46:12 -0300

    home grid

--- lib/pages/home/home_content.dart
+++ lib/pages/home/home_content.dart
@@ -10,12 +10,92 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) =>
       BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(
-            title: const Text('Hello Jhon'),
-          ),
-          body: SingleChildScrollView(
-            child: Container(),
-          ),
-        );
+            // appBar: AppBar(
+            //   title: const Text('Hello Jhon'),
+            // ),
+            body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.blue[500],
+              child: Row(
+                children: [
+                  RotatedBox(
+                    quarterTurns: 135,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: const Text('Discovery'),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 220,
+                      color: Colors.blue[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      RotatedBox(
+                        quarterTurns: 135,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 24,
+                          ),
+                          child: const Text('Ingredients'),
+                        ),
+                      ),
+                      RotatedBox(
+                        quarterTurns: 135,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 24,
+                          ),
+                          child: const Text('Category'),
+                        ),
+                      ),
+                      RotatedBox(
+                        quarterTurns: 135,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 24,
+                          ),
+                          child: const Text('Alcoholic'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Container(
+                          color: Colors.blue[300],
+                          height: 100,
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.blue[200],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+            // bottomNavigationBar: BottomNavigationBar(items: const []),
+            );
       });
 }
