0dc28664fb09521102332bccc6bc9af28ea71f68
gabriel-Oak <gabrielcarvalhocosta@live.com>
2022-12-11 20:48:37 -0300

    details

--- lib/pages/detail/detail_content.dart
+++ lib/pages/detail/detail_content.dart
@@ -3,6 +3,7 @@ import 'package:drink_it/pages/detail/bloc/detail_bloc.dart';
 import 'package:drink_it/pages/detail/bloc/detail_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletons/skeletons.dart';
 
 class DetailContent extends StatelessWidget {
   const DetailContent({super.key});
@@ -23,10 +24,150 @@ class DetailContent extends StatelessWidget {
             ),
           ),
           body: state is DetailLoaded
-              ? SingleChildScrollView(
-                  child: Column(
-                    children: [Text(state.cocktail.name)],
-                  ),
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      child: Stack(
+                        children: [
+                          const SkeletonAvatar(
+                            style: SkeletonAvatarStyle(
+                              width: double.infinity,
+                              height: double.infinity,
+                            ),
+                          ),
+                          Image(
+                            image:
+                                NetworkImage('${state.cocktail.thumb}/preview'),
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            left: 18,
+                            top: 18,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  color: Colors.white60,
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 2,
+                                    horizontal: 4,
+                                  ),
+                                  constraints: BoxConstraints.loose(
+                                      const Size(160, 200)),
+                                  child: Text(
+                                    state.cocktail.name,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    softWrap: true,
+                                    style: const TextStyle(
+                                      fontSize: 36,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 4),
+                                  color: Colors.white60,
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 2,
+                                    horizontal: 4,
+                                  ),
+                                  constraints: BoxConstraints.loose(
+                                      const Size(160, 200)),
+                                  child: Text(
+                                    state.cocktail.ingredients.first.name,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    softWrap: true,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 4),
+                                  color: Colors.white60,
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 2,
+                                    horizontal: 4,
+                                  ),
+                                  constraints: BoxConstraints.loose(
+                                      const Size(160, 200)),
+                                  child: Text(
+                                    state.cocktail.category ?? 'Unknown',
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    softWrap: true,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            RotatedBox(
+                              quarterTurns: 135,
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 16,
+                                    horizontal: 24,
+                                  ),
+                                  child: const Text(
+                                    'Description',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            RotatedBox(
+                              quarterTurns: 135,
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 16,
+                                    horizontal: 24,
+                                  ),
+                                  child: const Text(
+                                    'Ingredients',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SingleChildScrollView(
+                          child: Column(
+                            children: const [],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 )
               : const Center(child: CircularProgressIndicator()),
         );
